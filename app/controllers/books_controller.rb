85eec18904834d48e8faed566aaa353dbdd4f761
cocoared <dmm23program@gmail.com>
2022-02-21 17:55:40 +0000

    [Fix]Layouts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,48 @@
 class BooksController < ApplicationController
+
   def new
     @book = Book.new
   end
 
-  def index
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_index_path
   end
+
+  def index
+    @books = Book.all
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+
 
   def show
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/index'
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body, )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as: "about"
-  resources :books, only: [:new, :index, :show, :create, :destroy] do
+  resources :books, only: [:new, :index, :show, :create, :destroy, :update] do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :index, :show]
+  resources :users, only: [:edit, :index, :show, :update]
 end
